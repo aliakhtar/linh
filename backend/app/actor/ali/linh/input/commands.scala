a46a4b9496d89a27fe686f335c801aee47b422f0
Ali Akhtar <ali@ali.actor>
2020-11-21 17:19:36 +0500

    Figured out how to call external static methods from grammar / lambda

---
+++ backend/app/actor/ali/linh/input/commands.scala
@@ -0,0 +1,9 @@
+package actor.ali.linh.input
+
+import scala.collection.parallel.CollectionConverters._
+import scala.jdk.CollectionConverters._
+import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
+import com.typesafe.scalalogging.Logger
+
+case class ShowInventory()
